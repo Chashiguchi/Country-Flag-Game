e3c2380d541e12df66ad239a4421aa85ee1a9ee1
Chase Hashiguchi <chashiguchi@bsd220.org>
2024-01-18 09:31:17 -0600

    Created initial view

--- Country Flag Game/Views/ContentView.swift
+++ Country Flag Game/Views/ContentView.swift
@@ -8,30 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var quizManager = QuizManager()
+    @StateObject var gameManager = GameManager()
     var body: some View {
         NavigationView {
-            VStack(spacing: 40) {
-                VStack(spacing: 20) {
+            VStack(spacing: 40 ,content:  {
+                VStack(spacing: 20, content: {
                 Text("Country Flag Game")
                         .font(.title)
                         .fontWeight(.heavy)
                         .foregroundColor(.yellow)
                     Text("Ready to test your skillz?")
                         .foregroundColor(.yellow)
-                }
+                })
                 NavigationLink{
                     Question_View()
-                        .environmentObject(quizManager)
+                        .environmentObject(gameManager)
                 } label: {
                     CustomButton(text: "Start")
                 }
-            }
+            })
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .ignoresSafeArea(.all)
             .background(.cyan)
         }
-        .padding()
     }
 }
 
